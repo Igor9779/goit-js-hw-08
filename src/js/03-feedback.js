be9914f5d99fcc15b7bf32d2c521f5fc7a5cb792
Igor9779 <darthhoit97@gmail.com>
2023-07-11 21:03:15 +0300

    task-3-ex

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
 
+
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+};
+
+function onTextareaInput(e) {
+    const message = e.currentTarget.value;
+    localStorage.setItem('feedback-form-state', message);
+};
